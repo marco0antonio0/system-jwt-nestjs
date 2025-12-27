0fca892d0a3febd7246aa87693dab8fc5b82971f
marco0antonio0 <marcomesquitajr@hotmail.com>
2025-12-27 12:12:32 -0300

    fix: corrigir importação do AppController para usar health.controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { AppController } from './health.controller';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
